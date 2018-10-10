0e1274f5cec50c5c686b5604ed8271cf9239e88b
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-10 17:14:21 -0400

    added main game logic

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -22,9 +22,11 @@ class App extends Component {
 			"xeno.jpg",
 		],
 		clicked: [],
-		score: 0
+		score: 0,
+		hiscore: 0
 	}
 
+	//RANDOMIZE ORDER OF IMAGES
 	scrambleImages = () => {
 		let newArr = this.state.images;
 		for (let i = newArr.length - 1; i > 0; i--) {
@@ -36,12 +38,37 @@ class App extends Component {
 		this.setState({images: newArr});
 	};
 
+	//GAME LOGIC
+	progressGame = monster => {
+		if (this.state.clicked.indexOf(monster) === -1) { //if monster has not been clicked yet
+			this.setState({
+				clicked: [...this.state.clicked, monster], //add it to array of clicked monsters
+				score: this.state.score + 1 //increment score by 1
+			}, () => { //then
+				if (this.state.score > this.state.hiscore) { //if new score is higher then high score
+					this.setState({
+						hiscore: this.state.score //set new high score
+					});
+				}
+			});
+		} else { //if monster has already been clicked before
+			this.setState({ //reset game
+				clicked: [],
+				score: 0
+			});
+		}
+	}
+
+	//IMAGE CLICK EVENT
 	handleImageClick = event => {
-		this.scrambleImages();
+		this.scrambleImages(); //randomize order of images
+
+		//passes only monster name (without file extension) to game logic function
+		this.progressGame(event.target.dataset.image.split(".")[0]);
 	};
 
-	componentDidMount() {
-		this.scrambleImages();
+	componentDidMount() { //on page load
+		this.scrambleImages(); //randomize order of images
 	}
 
 	render() {
--- memory-game/src/components/Image/Image.js
+++ memory-game/src/components/Image/Image.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Image = props=> (
 	<div>
-		<img src={`images/${props.image}`} alt="" {...props}></img>
+		<img src={`images/${props.image}`} alt="" onClick={props.onClick} data-image={props.image}></img>
 	</div>
 );
 
