21c7286faa28bc791a55a936b65b74920e4d1137
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-10-11 14:06:12 -0400

    Updates

---
+++ precache-manifest.e575f29f4c6d7d4771dae34f0a39fe88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "a77845b298a9ef23465e",
+    "url": "/Memory-Game/static/js/main.a77845b2.chunk.js"
+  },
+  {
+    "revision": "94c8180847106eb35c9d",
+    "url": "/Memory-Game/static/js/1.94c81808.chunk.js"
+  },
+  {
+    "revision": "a77845b298a9ef23465e",
+    "url": "/Memory-Game/static/css/main.232f6931.chunk.css"
+  },
+  {
+    "revision": "14c8c21e8aef5d3bfb76ead3a882eedd",
+    "url": "/Memory-Game/index.html"
+  }
+];
